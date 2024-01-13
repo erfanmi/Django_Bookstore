b2578e7384f803c5d686e0165c5ecb10c9c6daef
erfanmi <erfanmirghiasi@gmail.com>
2024-01-13 19:02:02 +0330

    books detail and staff

--- books/models.py
+++ books/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
+
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
@@ -11,3 +13,6 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('book_detail', args=[self.id])
--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from .views import BookListView
+from .views import BookListView , BookDetailView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='books_list'),
+    path('<int:pk>', BookDetailView.as_view(), name='book_detail')
 ]
