79a5c66776f3464a6765706d26c1e326ae49ac72
erfanmi <erfanmirghiasi@gmail.com>
2024-01-20 00:27:28 +0330

    comments added to view and html files

--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.urls import reverse_lazy
 # Create your views here.
@@ -12,10 +12,14 @@ class BookListView(generic.ListView):
     context_object_name = 'books'
 
 
-class BookDetailView(generic.DetailView):
-    model = Book
-    template_name = 'books/book_detail.html'
-    context_object_name = 'book'
+# class BookDetailView(generic.DetailView):
+#     model = Book
+#     template_name = 'books/book_detail.html'
+#     context_object_name = 'book'
+def book_detail_view(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    book_comments = book.comments.all()
+    return render(request,'books/book_detail.html', {'book': book, 'comments': book_comments})
 
 
 class BookCreateView(generic.CreateView):
