636dd9f9e63235b0545dae9fb9a4bbc78bc5d883
erfanmi <erfanmirghiasi@gmail.com>
2024-01-28 01:47:45 +0330

    comments handled via comment_form

--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.urls import reverse_lazy
 # Create your views here.
+from .forms import CommentForm
 from .models import Book
 
 
@@ -19,7 +20,21 @@ class BookListView(generic.ListView):
 def book_detail_view(request, pk):
     book = get_object_or_404(Book, pk=pk)
     book_comments = book.comments.all()
-    return render(request,'books/book_detail.html', {'book': book, 'comments': book_comments})
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.book = book
+            new_comment.save()
+            comment_form = CommentForm()
+
+    else:
+        comment_form = CommentForm()
+    return render(request, 'books/book_detail.html',
+                  {'book': book,
+                   'comments': book_comments,
+                   'comment_form': comment_form})
 
 
 class BookCreateView(generic.CreateView):
